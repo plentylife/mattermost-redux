9b2b963b04fa429f0c52e4bf68ac05408a9a01cc
JoramWilander <jwawilander@gmail.com>
2017-10-31 11:23:19 -0400

    Add index.js for global import

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+import Actions from 'actions';
+import ActionTypes from 'action_types';
+import Client from 'client';
+import Constants from 'constants';
+import Reducers from 'reducers';
+import Selectors from 'selectors';
+import Store from 'store';
+import Utils from 'utils';
+
+export default {
+    Actions,
+    ActionTypes,
+    Client,
+    Constants,
+    Reducers,
+    Selectors,
+    Store,
+    Utils
+};
